06693ea20b8b8afdf97831b9eec82d58f59ff7a6
Erick Fernando <erikefernandes40@gmail.com>
2022-02-23 21:52:44 -0300

    adicao de arquivos js e arquivos html

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
-
 const server = express()
+const routes = require("./routes")
 
-server.get('/', () => {
-    
-})
+//habilitar arquivos estaticos
+
+server.use(express.static("public"))
+
+//routes
 
 server.listen(3000, () => console.log('rodando'))
